64104b1b01d5f61ba6da47f5fcbde3383accf1e2
kelvin <8999>
2022-02-24 19:17:33 +0800

    mvc debug

---
+++ maven/sprint_mvc/src/main/java/com/czl/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.czl.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String get() {
+        return "hello world";
+    }
+}
